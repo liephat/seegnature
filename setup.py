34e84e3efef677168b8150760309ab34c5a6e48e
Mike Imhof <mike.imhof@uni-bamberg.de>
2018-06-20 15:24:26 +0200

    add install requirements

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup
 
 setup(name='seegnature',
-      version='0.1',
-      description='Module for supporting detection of electrophysiological signatures',
-      url='https://github.com/liephat/seegnature',
-      author='Mike Imhof',
-      author_email='mike.imhof@uni-bamberg.de',
-      packages=['seegnature'],
-      zip_safe=False)
+    version='0.1.1',
+    description='Module for supporting detection of electrophysiological signatures',
+    url='https://github.com/liephat/seegnature',
+    author='Mike Imhof',
+    author_email='mike.imhof@uni-bamberg.de',
+    packages=['seegnature'],
+    install_requires=[
+        'numpy',
+        'tensorflow',
+        'tflearn'
+    ],
+    zip_safe=False)
