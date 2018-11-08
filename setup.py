5cfa53378b65ea6026f2a9e9b9936063a1613ae8
Mike Imhof <mike.imhof@uni-bamberg.de>
2018-11-08 12:06:36 +0100

    update package version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='seegnature',
-      version='0.2.3',
+      version='0.2.4',
       description='Module for supporting detection of electrophysiological signatures',
       url='https://github.com/liephat/seegnature',
       author='Mike Imhof',
